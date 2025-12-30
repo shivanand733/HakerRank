ea968e388254ba16515681d5e886d79b809f331d
shivanand733 <shivanandkumbar5604@gmail.com>
2025-12-30 23:59:04 +0530

    create the function and call it reduce the time

--- src/DSA_with_Kunal/sum.java
+++ src/DSA_with_Kunal/sum.java
@@ -1,7 +1,22 @@
 package DSA_with_Kunal;
 
+import java.util.Scanner;
+
 public class sum {
+	
 	public static void main(String[] args) {
+		sum();
+		sum();
+	}
+	
+	static void sum() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter A and B value");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = 0;
+		System.out.println("Sum of a + b = "+ (c = a + b));
+		System.out.println();
 		
 	}
 }
