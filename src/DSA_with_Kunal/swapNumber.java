109c50556fa52d66a65e950400f946a0ca4bfe4e
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-04 23:08:24 +0530

    --

--- src/DSA_with_Kunal/swapNumber.java
+++ src/DSA_with_Kunal/swapNumber.java
@@ -2,19 +2,18 @@ package DSA_with_Kunal;
 
 public class swapNumber {
 public static void main(String[] args) {
-	int a = 10;
-	int b = 20;
 	
-	swap(a, b);
-	
-	System.out.println("The a is " + a  + " and b is " + b);
+	swap();
+
 }
 
-public static void swap(int a, int b) {
+public static void swap() {
+	int a = 10;
+	int b = 20;
 	int temp = a;
 	a = b; 
 	b = temp;
-	
+	System.out.println(a +" "+ b);
 	
 }
 }
