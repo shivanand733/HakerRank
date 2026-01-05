207bab9d1967a96db2f68df0279aa76c854ea563
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-05 13:49:32 +0530

    create simple pattrens

---
+++ src/pattren/invertTraiangle.java
@@ -0,0 +1,20 @@
+package pattren;
+
+public class invertTraiangle {
+public static void main(String[] args) {
+	for (int i = 1; i <= 5; i++) {
+		
+		for (int k = 1; k <= i;k++) {
+			System.out.print(" ");
+		}
+		for (int j = 5; j >= i; j--) {
+			System.out.print("* ");
+		}
+		
+		
+		System.out.println();
+	}
+	
+	
+}
+}
