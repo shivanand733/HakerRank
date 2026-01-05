0662fcfb0024ed9e3a2175dd38cf2494da49bee8
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-05 13:13:30 +0530

    create a simple pattern

---
+++ src/pattren/StarLight.java
@@ -0,0 +1,13 @@
+package pattren;
+
+public class StarLight {
+public static void main(String[] args) {
+	for (int i = 1; i <= 5; i++) {
+		if (i == 3) {
+		System.out.println(" ");
+	} else {
+		System.out.println("*");
+	}
+	}
+}
+}
