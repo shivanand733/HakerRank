2e53c84b015efd2876fa74823c24e7a19731f444
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-05 11:45:06 +0530

    create the Twinkling Star pattren

---
+++ src/pattren/twinklingStar.java
@@ -0,0 +1,13 @@
+package pattren;
+
+public class twinklingStar {
+public static void main(String[] args) {
+	
+	for(int i = 1; i <= 4; i++) {
+		if (i%2 == 1) {
+			System.out.println("*");
+		} else {
+		}
+	}
+}
+}
