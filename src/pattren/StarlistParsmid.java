a3a31cd0fe69b77bf1bacfd868b917b464f1e897
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-05 12:22:30 +0530

    ctrate the Star Ptter

--- src/pattren/StarlistParsmid.java
+++ src/pattren/StarlistParsmid.java
@@ -2,12 +2,15 @@ package pattren;
 
 public class StarlistParsmid {
 public static void main(String[] args) {
-	for (int i = 1; i <= 5; i++) {
-		for (int j = 1; j <= i; j++)  {
+	int row = 5;
+	
+	for (int i = 1; i <= row; i++) {
+		for (int j = 1; j <= row-i; j++) {
+			System.out.print(" ");
+		} for (int k = 1; k <= 2*i-1; k++) {
 			System.out.print("*");
 		}
-		System.out.println("-");
+		System.out.println();
 	}
-	System.out.println();
 }
 }
