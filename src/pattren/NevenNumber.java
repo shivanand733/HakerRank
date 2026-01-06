28ca8a4e32618bfb9f7df0006054aa21fc8d86d5
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-06 18:19:13 +0530

    create the n Even Number

---
+++ src/pattren/NevenNumber.java
@@ -0,0 +1,18 @@
+package pattren;
+
+import java.util.Scanner;
+
+public class NevenNumber {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+
+	for (int i = 1; i <= n;i++) {
+		System.out.println(2 * i);
+	}
+	
+
+
+}
+
+}
