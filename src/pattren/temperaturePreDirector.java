93f711011db70c7fcd53d982f990c51267b62b62
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-06 20:17:31 +0530

    wap Temparature mager

--- src/pattren/temperaturePreDirector.java
+++ src/pattren/temperaturePreDirector.java
@@ -1,22 +1,30 @@
 package pattren;
 
-import java.util.*;
+import java.util.Scanner;
+
 public class temperaturePreDirector {
-public static void main(String[] args) {
-	
-	Scanner scan = new Scanner(System.in);
-	int temp = scan.nextInt();
-	
-	if (temp < 0) {
-		 System.out.println("The temperature " + temp + " is categorized as Freezing.");
-	} else if (temp > 0 || temp < 15 ) {
-		 System.out.println("The temperature " + temp + " is categorized as Freezing.");
-	} else if (temp > 16 || temp < 25) {
-		 System.out.println("The temperature " + temp + " is categorized as Freezing.");
-	} else if (temp > 26 || temp < 35) {
-		System.out.println();
-	} else {
-		System.out.println("Hot");
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Tempareature...");
+		int temp = scan.nextInt();
+		
+		if (temp < 0) {
+			System.out.println("The Temperature " + temp + " is categorized as Freezing.");
+		} else if (temp >= 0 && temp <= 15) {
+			System.out.println("The Temperature " + temp + " is categorized as Cool");
+		} 
+		
+		else if (temp >= 16 && temp <= 25) {
+			System.out.println("The Temperature " + temp + " is categorized as Modreate");
+		}
+		
+		
+		else if (temp >= 26 && temp <= 35) {
+			System.out.println("The Temperature " + temp + " is categorized as Warm");
+		} else {
+			System.out.println("The Temperature " + temp + " is categorized as Hot");
+
+		}
 	}
-}
 }
