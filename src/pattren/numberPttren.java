9751f3146355d26b8293132e708f28dc107ba138
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-06 12:32:22 +0530

    create the pattren program

---
+++ src/pattren/numberPttren.java
@@ -0,0 +1,12 @@
+package pattren;
+
+public class numberPttren {
+public static void main(String[] args) {
+	for (int i = 5; i >= 1; i--) {
+		for (int j = 5; j >= i; j--) {
+			System.out.print(j + " ");
+		}
+		System.out.println();
+	}
+}
+}
