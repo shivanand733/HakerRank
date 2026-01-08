03a536d610889194c00d609a5a3bab7f85105a6a
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-08 09:58:14 +0530

    WAP enter the name

--- src/pattren/helloWorld.java
+++ src/pattren/helloWorld.java
@@ -1,7 +1,12 @@
 package pattren;
 
+import java.util.Scanner;
+
 public class helloWorld {
 public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter Name ");
 	
+	String name = scan.nextLine();
 }
 }
