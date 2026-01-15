14032864c99deda95f38776764ea7683d1e30ad9
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-15 16:05:21 +0530

    WAP of Arrays

--- src/DSA_with_Kunal/ShivArray.java
+++ src/DSA_with_Kunal/ShivArray.java
@@ -8,8 +8,24 @@ public static void main(String[] args) {
 	System.out.println("Enter Size of Array");
 	int size = scan.nextInt();
 	
-	System.out.println("Enter Arrays");
-	int [] array = new int[size];
+	int arry[] = new int[size];
+	System.out.println("Enter the arrys size");
+	for (int i = 0; i < arry.length;i++) {
+		arry[i] = scan.nextInt();
+	}
+	
+	System.out.println("The Arrys are ");
+	System.out.print("[");
+	for (int i = 0; i < arry.length; i++) {
+		System.out.print(arry[i]);
+	
+		if (i != arry.length -1) {
+			System.out.print(",");
+		}
+ 	} 
+	System.out.println("]");
+
+	
 	
 }
 }
