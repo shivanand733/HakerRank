f037ba0595aafe8734f25203a9ee69da7b636c7b
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-16 09:45:53 +0530

    create a simple project on the Arrays

---
+++ src/DSA_with_Kunal/passingFunctions.java
@@ -0,0 +1,15 @@
+package DSA_with_Kunal;
+
+import java.util.Arrays;
+
+public class passingFunctions {
+public static void main(String[] args) {
+	
+	int[] nums = {2,4,45,98};
+	
+	System.out.println(Arrays.toString(nums));
+}
+public static void change(int[] arry) {
+	
+}
+}
