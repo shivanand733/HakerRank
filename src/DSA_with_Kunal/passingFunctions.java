95e2486887ef8e69c93e07212d83c51eb8e5a99e
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-16 09:49:45 +0530

    create a Simple Arryas Program

--- src/DSA_with_Kunal/passingFunctions.java
+++ src/DSA_with_Kunal/passingFunctions.java
@@ -7,9 +7,12 @@ public static void main(String[] args) {
 	
 	int[] nums = {2,4,45,98};
 	
-	System.out.println(Arrays.toString(nums));
+	System.out.println("Original Array "+ Arrays.toString(nums));
+	change(nums);
+	System.out.println("Change The Array "+Arrays.toString(nums));
 }
 public static void change(int[] arry) {
-	
+	arry[0] = 89;
 }
+
 }
