02581a22c2b685326d703765b8d05443df9b9ce3
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-17 14:40:10 +0530

    2d Array

--- src/DSA_with_Kunal/multiDimetionArray.java
+++ src/DSA_with_Kunal/multiDimetionArray.java
@@ -1,5 +1,27 @@
 package DSA_with_Kunal;
 
-public class multiDimetionArray {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class multiDimetionArray {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter The Rows");
+	int rows = scan.nextInt();
+	
+	int [][] array = new int[rows][];
+	
+	for (int i = 0; i < array.length; i++) {
+		for (int j = 0; j < array[i].length;j++) {
+			array[i][j] = scan.nextInt();
+		}
+	}
+	
+	for (int i = 0; i < array.length; i++) {
+		for (int j = 0; j < array[i].length;j++) {
+			System.out.println(Arrays.toString(array));
+		}
+	}
+}
 }
