9166db478eabf3b6948c919ee826fdfb51aa33cc
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-19 22:34:08 +0530

    2D arrays

--- src/DSA_with_Kunal/twoDarray.java
+++ src/DSA_with_Kunal/twoDarray.java
@@ -1,7 +1,15 @@
 package DSA_with_Kunal;
 
+import java.util.Scanner;
+
 public class twoDarray {
 public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the size of the array ");
+	int size = scan.nextInt();
 	
+	int[][] arry = new int[size][];
+	
+
 }
 }
