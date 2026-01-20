6160d4fb850b0e7635665288ab8cd815853e32d9
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-20 23:06:05 +0530

    create a 2D arrys

--- src/DSA_with_Kunal/twoDarray.java
+++ src/DSA_with_Kunal/twoDarray.java
@@ -6,10 +6,18 @@ public class twoDarray {
 public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 	System.out.println("Enter the size of the array ");
-	int size = scan.nextInt();
+	int rows = scan.nextInt();
 	
-	int[][] arry = new int[size][];
+	int[][] arry = new int[rows][];
 	
+	//takeing the colum size and eliment
+	
+	for(int i = 0; i < rows; i++) {
+		System.out.println("Enter the numbers of colums for rows" + i + " : ");
+		int cols = scan.nextInt();
+		
+		arry[i] = new int[cols];
+	}
 
 }
 }
